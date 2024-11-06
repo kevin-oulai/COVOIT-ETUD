eb51bba62c53962db46ed10f0f9343353609af87
Thibault Rosalie <trosalie@iutbayonne.univ-pau.fr>
2024-11-06 15:30:55 +0100

    Mise a jour de la classe VoitureDao

--- modeles/voiture.dao.php
+++ modeles/voiture.dao.php
@@ -1,1 +1,19 @@
 <?php
+class VoitureDao {
+    private ?PDO $PDO;
+
+    public function __construct(?PDO $pdo = null)
+    {
+        $this->PDO = $pdo;
+    }
+
+    public function getPDO(): ?PDO
+    {
+        return $this->PDO;
+    }
+
+    public function setPDO(?PDO $PDO): void
+    {
+        $this->PDO = $PDO;
+    }
+}
