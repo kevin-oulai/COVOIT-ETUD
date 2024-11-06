eacf9c84440dba05cc0dc3ab729cef876b2eca49
Thibault Rosalie <trosalie@iutbayonne.univ-pau.fr>
2024-11-06 15:26:06 +0100

    Ajout du modele de la classe Voiture et du modele DAO

---
+++ modeles/voiture.class.php
@@ -0,0 +1,47 @@
+<?php
+
+class Voiture {
+
+    // Attributs
+    private int $numero;
+    private string|null $nom;
+    private string|null $marque;
+    private int $nombrePlaces;
+
+    // Constructeur
+    public function __construct(?int $pNumero, ?string $pNom=null, ?string $pMarque=null, ?int $pNombrePlaces) {
+        $this -> setNumero($pNumero);
+        $this -> setNom($pNom);
+        $this -> setMarque($pMarque);
+        $this -> setNombrePlaces($pNombrePlaces);
+    }
+
+    // Getters & Setters
+        // Getters
+    public function getNumero(): ?int {
+        return $this -> numero;
+    }
+    public function getNom(): ?string {
+        return $this -> nom;
+    }
+    public function getMarque(): ?string {
+        return $this -> marque;
+    }
+    public function getNombrePlaces(): ?int {
+        return $this -> nombrePlaces;
+    }
+
+        // Setters
+    public function setNumero($pNumero): void {
+        $this -> numero = $pNumero;
+    }
+    public function setNom($pNom): void {
+        $this -> nom = $pNom;
+    }
+    public function setMarque($pMarque): void {
+        $this -> marque = $pMarque;
+    }
+    public function setNombrePlaces($pNombrePlaces): void {
+        $this -> nombrePlaces = $pNombrePlaces;
+    }
+}
