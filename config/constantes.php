507b8dedd7c50ba06a3800b8492423da28071506
Kevin Oulai <koulai001@univ-pau.fr>
2024-11-06 15:35:12 +0100

    Creation de la class BD et de ses constantes

---
+++ config/constantes.php
@@ -0,0 +1,8 @@
+<?php
+
+
+//Connexion à la base de données
+define('DB_HOST', 'lakartxela.iutbayonne.univ-pau.fr');
+define('DB_NAME', 'tbirembeaux_pro');
+define('DB_USER', 'tbirembeaux_pro');
+define('DB_PASS', 'tbirembeaux_pro');
