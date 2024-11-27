61861e35a153b9b62bf65263328707e9f5d21fd1
Titouan Galles <tgalles@iutbayonne.univ-pau.fr>
2024-11-27 15:14:21 +0100

    premiere interface de la page des trajet avec toutes les infos en fonction de ce qui est entrer en parametre

--- controllers/controller_trajet.class.php
+++ controllers/controller_trajet.class.php
@@ -15,15 +15,34 @@ class ControllerTrajet extends Controller{
         // $date = $_POST['date'];
         // $nbPassager = $_POST['nombre_passagers'];
 
-        $managerTrajet = new LieuDao($this->getPdo());
-        $numTrajet1 = $managerTrajet->findNumByVille("Anglet");
-        $numTrajet2 = $managerTrajet->findNumByVille("Paris");
-        var_dump($numTrajet1);
-        var_dump($numTrajet2);
+        $managerLieu = new LieuDao($this->getPdo());
+        $numTrajet1 = $managerLieu->findNumByVille("Anglet");
+        $numTrajet2 = $managerLieu->findNumByVille("Paris");
+        $listeNum1="(";
+        $listeNum2="(";
+        
+        foreach($numTrajet1 as $num1)
+        {
+            $listeNum1 = $listeNum1 . $num1['numero'] . ", ";
+            
+        }
+        $listeNum1 = substr($listeNum1, 0, -2);
+        $listeNum1 = $listeNum1 . ")";
+        foreach($numTrajet2 as $num2)
+        {
+            $listeNum2 = $listeNum2 . $num2['numero'] . ", ";
+            
+        }
+        $listeNum2 = substr($listeNum2, 0, -2);
+        $listeNum2 = $listeNum2 . ")";
+        
+        $managerTrajet = new TrajetDao($this->getPdo());
+        $listeTrajet = $managerTrajet->findAll($listeNum1, $listeNum2, "2024-11-06", 1);
 
         $template = $this->getTwig()->load('pageTrajets.html.twig');
 
         echo $template->render(array(
+            'listeTrajet' => $listeTrajet,
         ));
         
    
@@ -32,7 +51,6 @@ class ControllerTrajet extends Controller{
     public function rechercher(){
         $managerEtudiant = new EtudiantDao($this->getPdo());
         $etudiant = $managerEtudiant->find(1);
-        //var_dump($etudiant);
 
         $template = $this->getTwig()->load('index.html.twig');
 
--- modeles/trajet.dao.php
+++ modeles/trajet.dao.php
@@ -62,19 +62,19 @@ class TrajetDao{
         return $trajet;
     }
 
-    public function findAll(string $lieu_depart, string $lieu_arrivee, string $date, int $nbPassager): Trajet
+    public function findAll(string $num_lieu_depart, string $num_lieu_arrivee, string $date, int $nbPassager): array
     {
-        $requete = "SELECT numero FROM LIEU WHERE ville = :lieu_depart";
+        $requete = "SELECT T.heureDep, T.heureArr, T.prix, T.dateDep, T.nbPlace, L1.ville AS villeArr, L2.ville AS villeDep FROM TRAJET T LEFT JOIN LIEU L1 ON L1.numero = T.numero_lieu_arrivee LEFT JOIN LIEU L2 ON L2.numero = T.numero_lieu_depart WHERE numero_lieu_depart IN " . $num_lieu_depart . " AND numero_lieu_arrivee IN " . $num_lieu_arrivee . " AND dateDep = ? AND nbPlace > " . $nbPassager . "";
         $pdoStatement = $this->PDO->prepare($requete);
-        $pdoStatement->execute(array(":lieu_depart"=>$lieu_depart));
-        $pdoStatement->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Lieu');
-        $numLieuDep = $pdoStatement->fetch();
+        $pdoStatement->bindValue(1, $date, PDO::PARAM_STR);
+        $pdoStatement->execute();
+        $listeTrajet = $pdoStatement->fetchAll();
+        return $listeTrajet;
+    }
 
-        $requete = "SELECT numero FROM LIEU WHERE ville = :lieu_depart";
-        $pdoStatement = $this->PDO->prepare($requete);
-        $pdoStatement->execute(array(":lieu_depart"=>$lieu_depart, ":lieu_arrivee"=>$lieu_arrivee, ":uneDate"=>$date, ":nbPlace"=>$nbPassager));
-        $pdoStatement->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Trajet');
-        $trajet = $pdoStatement->fetch();
-        return $trajet;
+    public function hydrate(array $tab): Trajet
+    {
+        $trajet = new Trajet();
+        
     }
 }
