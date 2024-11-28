8f97e84b7937628eec06e99439390d8528c01c96
Candice Dutournier <cdutourni001@iutbayonne.univ-pau.fr>
2024-11-28 09:23:09 +0100

    affichage de donnees simples (voiture, avis et badge non lié à l'étudiant)

--- controllers/controller_etudiant.class.php
+++ controllers/controller_etudiant.class.php
@@ -6,11 +6,33 @@ class ControllerEtudiant extends Controller{
     }
 
     public function afficher(){
+        $managerEtudiant = new EtudiantDao($this->getPdo());
+        $managerVoiture = new VoitureDao($this->getPdo());
+        $managerBadge = new BadgeDao($this->getPdo());
+        $managerAvis = new AvisDao($this->getPdo());
+        $etudiant = $managerEtudiant->find(1);
+        $voiture = $managerVoiture->find(1);
+        $badge = $managerBadge->find(1);
+        $avis = $managerAvis->find(1);
+        //var_dump($etudiant);
         
         $template = $this->getTwig()->load('profil.html.twig');
 
         echo $template->render(array(
+            'etudiant' => $etudiant,
+            'voiture' => $voiture,
+            'badge' => $badge,
+            'avis' => $avis,
         ));
+
+        $numero_conducteur = 1; // A changer lorsque les variables de session serons mises en place.
+        if($numero_conducteur == 1) {
+            
+        }
+        else {
+            //redirection page connexion
+        }
+
     }
     
 }
