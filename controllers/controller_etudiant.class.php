68c5c5a8bf3128f3068d99ef5e6ab6ef57399ba8
Candice Dutournier <cdutourni001@iutbayonne.univ-pau.fr>
2024-12-02 16:33:58 +0100

    maj bonne photo profil + mise en place partiellement fonctionnelle des if else de profil.html.twig

--- controllers/controller_etudiant.class.php
+++ controllers/controller_etudiant.class.php
@@ -9,37 +9,33 @@ class ControllerEtudiant extends Controller{
         $num_etudiant=1;
         $managerEtudiant = new EtudiantDao($this->getPdo());
         $etudiant = $managerEtudiant->find($num_etudiant);
-        if(true){
-            $managerEtudiantConcerne = new EtudiantDao($this->getPdo());
-            $managerEtudiantCommentateur = new EtudiantDao($this->getPdo());
-            $managerNbTrajet = new EtudiantDao($this->getPdo());
-            $managerVoiture = new VoitureDao($this->getPdo());
-            $managerBadge = new BadgeDao($this->getPdo());
-            $managerAvisDonnes = new AvisDao($this->getPdo());
-            $managerAvisReçus = new AvisDao($this->getPdo());
-            $etudiantConcerne = $managerEtudiantConcerne->findConcerneParAvis($num_etudiant);
-            $etudiantCommentateur = $managerEtudiantCommentateur->findCommentateurDAvis($num_etudiant);
-            $nbTrajet = $managerNbTrajet->findNbTrajets($num_etudiant);
-            $voiture = $managerVoiture->findMonEtudiant($num_etudiant);
-            $badge = $managerBadge->find($num_etudiant);
-            $avisDonnes = $managerAvisDonnes->findCommentateur($num_etudiant);
-            $avisReçus = $managerAvisReçus->findConcerne($num_etudiant);
-            //var_dump($avis);
-        }
+        $managerEtudiantConcerne = new EtudiantDao($this->getPdo());
+        $managerEtudiantCommentateur = new EtudiantDao($this->getPdo());
+        $managerNbTrajet = new EtudiantDao($this->getPdo());
+        $managerVoiture = new VoitureDao($this->getPdo());
+        $managerBadge = new BadgeDao($this->getPdo());
+        $managerAvisDonnes = new AvisDao($this->getPdo());
+        $managerAvisReçus = new AvisDao($this->getPdo());
+        $etudiantConcerne = $managerEtudiantConcerne->findConcerneParAvis($num_etudiant);
+        $etudiantCommentateur = $managerEtudiantCommentateur->findCommentateurDAvis($num_etudiant);
+        $nbTrajet = $managerNbTrajet->findNbTrajets($num_etudiant);
+        $voiture = $managerVoiture->findMonEtudiant($num_etudiant);
+        $badge = $managerBadge->find($num_etudiant);
+        $avisDonnes = $managerAvisDonnes->findCommentateur($num_etudiant);
+        $avisReçus = $managerAvisReçus->findConcerne($num_etudiant);
+        //var_dump($avis);
         
         $template = $this->getTwig()->load('profil.html.twig');
 
         echo $template->render(array(
             'etudiant' => $etudiant,
-            //if($num_etudiant==1) {
-                'voiture' => $voiture,
-                'badge' => $badge,
-                'avisDonnes' => $avisDonnes,
-                'avisReçus' => $avisReçus,
-                'etudiantConcerne' => $etudiantConcerne,
-                'etudiantCommentateur' => $etudiantCommentateur,
-                'nbTrajet' => $nbTrajet,
-            //}
+            'voiture' => $voiture,
+            'badge' => $badge,
+            'avisDonnes' => $avisDonnes,
+            'avisReçus' => $avisReçus,
+            'etudiantConcerne' => $etudiantConcerne,
+            'etudiantCommentateur' => $etudiantCommentateur,
+            'nbTrajet' => $nbTrajet,
         ));
 
         $numero_conducteur = 1; // A changer lorsque les variables de session serons mises en place.
