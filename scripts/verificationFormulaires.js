717fd686c6fe97dde94258973b29fdb1c4509ee7
Kevin Oulai <koulai001@univ-pau.fr>
2025-01-23 13:42:46 +0100

    Ajout de verification js sur les champs d'adresse pour l'ergonomie

---
+++ scripts/verificationFormulaires.js
@@ -0,0 +1,14 @@
+function verifierAdresse(id){
+    let champ = document.getElementById(id);
+    let regexp = new RegExp("^[0-9]{1,3}\\s[a-zA-Z\\s\u00C0-\u017F]+,[a-zA-Z\\s-]+$");
+    if(regexp.test(champ.value)){
+        document.querySelector("."+id+"MessageErreur").innerHTML = "";
+        champ.classList.remove("invalid-input");
+        champ.classList.add("valid-input");
+    }
+    else{
+        document.querySelector("."+id+"MessageErreur").innerHTML = "Veuillez respecter le format d'adresse (ex : 1 Rue Paul, Paris)";
+        champ.classList.remove("valid-input");
+        champ.classList.add("invalid-input");
+    }
+}
