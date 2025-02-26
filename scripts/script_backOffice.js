535a21b372dfd9acb6cbaf53eda1f52aaad7b067
Candice Dutournier <cdutourni001@iutbayonne.univ-pau.fr>
2025-02-26 16:45:42 +0100

    Navigation dans les 3 pages du back office ok

---
+++ scripts/script_backOffice.js
@@ -0,0 +1,33 @@
+function openBackOffice(x) {
+    let contents = document.querySelectorAll(".tab-content");
+    let btns = document.getElementsByName("btnBackOffice"); 
+    for (let i = 0; i < contents.length; i++) {
+        contents[i].style.display = "none";
+        btns[i].classList.remove("btn");
+        btns[i].classList.remove("btn-primary");
+        btns[i].classList.remove("btn");
+        btns[i].classList.remove("btn-outline-primary");
+    }
+    contents[x].style.display = "block";
+    console.log(btns);
+    btns[x].classList.add("btn");
+    btns[x].classList.add("btn-primary");
+    if (x==0) {
+        btns[1].classList.add("btn");
+        btns[1].classList.add("btn-outline-primary"); 
+        btns[2].classList.add("btn");
+        btns[2].classList.add("btn-outline-primary"); 
+    }
+    else if(x==1){
+        btns[0].classList.add("btn");
+        btns[0].classList.add("btn-outline-primary"); 
+        btns[2].classList.add("btn");
+        btns[2].classList.add("btn-outline-primary"); 
+    }
+    else {
+        btns[0].classList.add("btn");
+        btns[0].classList.add("btn-outline-primary"); 
+        btns[1].classList.add("btn");
+        btns[1].classList.add("btn-outline-primary");
+    }
+}
