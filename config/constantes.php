954dda47a77cb23c90d688627093e3919585ca5a
Kevin <koulai001@univ-pau.fr>
2025-02-28 20:25:31 +0100

    Reecriture protection des pages conducteur et infos conducteur sur le recap du trajet

--- config/constantes.php
+++ config/constantes.php
@@ -2,8 +2,8 @@
 include 'modeles/etudiant.dao.php';
 include 'modeles/etudiant.class.php';
 //Connexion à la base de données
-define('DB_HOST', 'lakartxela.iutbayonne.univ-pau.fr');
-define('DB_NAME', 'koulai001_pro');
-define('DB_USER', 'koulai001_pro');
-define('DB_PASS', 'koulai001_pro');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'covoit-etud');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 session_start();
